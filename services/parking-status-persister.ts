ed449daf08853c836d6481a84124abe0e5533280
Franklyn Vieira <frankfhurt@gmail.com>
2018-07-27 21:11:47 -0300

    Refactor persistense lamba to persist or update the parkingStatus

--- services/parking-status-persister.ts
+++ services/parking-status-persister.ts
@@ -13,20 +13,52 @@ export class ParkingStatusPersister {
     public async save(parkingStatus: any, db: Db): Promise<any> {
         return new Promise<any>((resolve, reject) => {
 
-            // Fetch a collection
             let collection = db.collection('parkingStatus');
 
-            console.log("Will perform the insertion...")
+            console.log("Will perform the insertion/update...")
 
-            collection.insertOne(parkingStatus, (err, result) => {
+            const myquery = { parkingId: parkingStatus.parkingId };
+
+            collection.findOne(myquery, (err, result) => {
                 if (err != null) {
-                    console.error(`${TAG} an error occurred in save`, err);
+                    console.error(`${TAG} an error occurred in FindOne`, err);
                     reject(err);
                 } else {
-                    console.log(`${TAG} parkings status inserted! id: ${result.insertedId}`);
-                    resolve(result);
+                    console.log(`Result: ${result}`)
+                    console.log(`Result Stringfy: ${JSON.stringify(result)}`)
+                    if (result) {
+                        collection.updateOne({ _id: result._id }, { $set: parkingStatus }, (updateErr, updateResult) => {
+                            if (updateErr != null) {
+                                console.error(`${TAG} an error occurred in UPDATE`, updateErr);
+                                reject(updateErr);
+                            } else {
+                                console.log(`${TAG} parkings status UPDATED! id: ${updateResult}`);
+                                resolve(updateResult);
+                            }
+                        });
+                    } else {
+                        collection.insertOne(parkingStatus, (insertErr, insertResult) => {
+                            if (insertErr != null) {
+                                console.error(`${TAG} an error occurred in INSERT`, insertErr);
+                                reject(insertErr);
+                            } else {
+                                console.log(`${TAG} parkings status INSERTED! id: ${insertResult.insertedId}`);
+                                resolve(insertResult);
+                            }
+                        });
+                    }
                 }
             });
+
+            // collection.insertOne(parkingStatus, (err, result) => {
+            //     if (err != null) {
+            //         console.error(`${TAG} an error occurred in save`, err);
+            //         reject(err);
+            //     } else {
+            //         console.log(`${TAG} parkings status inserted! id: ${result.insertedId}`);
+            //         resolve(result);
+            //     }
+            // });
         });
     }
 }
